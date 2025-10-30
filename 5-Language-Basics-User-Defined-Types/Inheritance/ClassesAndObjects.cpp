7c958e994d08de29637bfa0a00f69e87b203bff3
Marcus Isaksson <me@naknut.email>
2025-10-30 12:34:37 +0100

    Add Inheritance demo

---
+++ 5-Language-Basics-User-Defined-Types/Inheritance/ClassesAndObjects.cpp
@@ -0,0 +1,17 @@
+#include "Tweeter.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+    Person p1("Marcus", "Isaksson", 123);
+    {
+        Tweeter t1("Someone", "Else", 456, "@whoever");
+        std::string name2 = t1.getName();
+    }
+    cout << "after innermost block" << endl;
+    string name = p1.getName();
+
+    return 0;
+}
