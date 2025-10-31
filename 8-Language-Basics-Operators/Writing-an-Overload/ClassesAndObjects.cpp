20bf3b19378f3f5e52c1a468cab746a3f6996430
Marcus Isaksson <me@naknut.email>
2025-10-31 13:52:45 +0100

    Add Writing an Overload demo

---
+++ 8-Language-Basics-Operators/Writing-an-Overload/ClassesAndObjects.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+#include "Person.h"
+
+int main() {
+    Person p1("Kate", "Gregory", 123);
+    Person p2("Someone", "Else", 456);
+    cout << "p1 is ";
+    if (!(p1 < p2)) {
+        cout << "not ";
+    }
+    cout << "less than p2" << endl;
+
+    cout << "p1 is ";
+    if (!(p1 < 300)) {
+        cout << "not ";
+    }
+    cout << "less than 300" << endl;
+
+    cout << "300 is ";
+    if (!(300 < p1)) {
+        cout << "not ";
+    }
+    cout << "less than p1" << endl;
+
+    return 0;
+}
---
+++ 8-Language-Basics-Operators/Writing-an-Overload/Person.cpp
@@ -0,0 +1,29 @@
+#include "Person.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+Person::Person(std::string first, std::string last, int arbitrary)
+    : firstname(first), lastname(last), arbitrarynumber(arbitrary) {
+        cout << "constructing " << GetName() << endl;
+    }
+
+Person::~Person() {
+    cout << "destructing " << GetName() << endl;
+}
+
+std::string Person::GetName() const {
+    return firstname + " " + lastname;
+}
+
+bool Person::operator<(const Person& p) const {
+    return arbitrarynumber < p.arbitrarynumber;
+}
+
+bool Person::operator<(int i) const {
+    return arbitrarynumber < i;
+}
+
+bool operator<(int i, const Person& p) {
+    return i < p.arbitrarynumber;
+}
